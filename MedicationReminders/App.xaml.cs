aec55cdc2643674fd4bb16ef2ff74ea32e2baf58
Michael Blades <michaelblades12@gmail.com>
2021-08-03 07:55:28 -0500

    Added SQLite database
    * Started model for database

--- MedicationReminders/App.xaml.cs
+++ MedicationReminders/App.xaml.cs
@@ -6,11 +6,22 @@ namespace MedicationReminders
 {
     public partial class App : Application
     {
+
+        public static string DatabaseLocation = string.Empty;
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            DatabaseLocation = databaseLocation;
         }
 
         protected override void OnStart()
---
+++ MedicationReminders/Model/Medication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MedicationReminders.Model
+{
+    public class Medication
+    {
+        public string Name { get; set; }
+
+        public string Schedule { get; set; }
+
+        public string DateTime { get; set; }
+
+    }
+}
